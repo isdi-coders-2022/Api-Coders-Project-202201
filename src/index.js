f6edbced2ad416bf3dd6553054b6342b8f151f6f
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-02-07 19:34:44 +0100

    wrapped App in index.js with the CocktailDataContextProvider

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import CocktailDataContextProvider from "./contexts/CocktailDataContextProvider";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CocktailDataContextProvider>
+      <App />
+    </CocktailDataContextProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function

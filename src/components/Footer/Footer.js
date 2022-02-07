9a8868f35ad1e7cf5a21a04ff0626bc9048d0ee4
Inna Iazovkaia <inna.0961@mail.ru>
2022-02-07 21:19:26 +0100

    changed class to className at Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,73 +1,78 @@
+import styled from "styled-components";
+
 const Footer = () => {
   return (
-    <ul class="links">
-      <li class="links__social">
-        <ul class="social-media">
+    <ul className="links">
+      <li className="links__social">
+        <ul className="social-media">
           <li>
             <a
-              class="social-media__fasebook media-icon"
+              className="social-media__fasebook media-icon"
               href="https://www.facebook.com/"
             >
-              <i class="fab fa-facebook-f"></i>
+              <i className="fab fa-facebook-f"></i>
             </a>
           </li>
           <li>
             <a
-              class="social-media__twitter media-icon"
+              className="social-media__twitter media-icon"
               href="https://twitter.com/"
             >
-              <i class="fab fa-twitter"></i>
+              <i className="fab fa-twitter"></i>
             </a>
           </li>
           <li>
             <a
-              class="social-media__linked-in media-icon"
+              className="social-media__linked-in media-icon"
               href="https://www.linkedin.com"
             >
-              <i class="fab fa-linkedin-in"></i>
+              <i className="fab fa-linkedin-in"></i>
             </a>
           </li>
           <li>
             <a
-              class="social-media__instagram media-icon"
+              className="social-media__instagram media-icon"
               href="https://www.instagram.com"
             >
-              <i class="fab fa-instagram"></i>
+              <i className="fab fa-instagram"></i>
             </a>
           </li>
           <li>
             <a
-              class="social-media__youtube media-icon"
+              className="social-media__youtube media-icon"
               href="https://www.youtube.com"
             >
-              <i class="fab fa-youtube"></i>
+              <i className="fab fa-youtube"></i>
             </a>
           </li>
         </ul>
       </li>
-      <li class="links__join">
+      <li className="links__join">
         <a href="https://www.isdicoders.com">Join us on Social Media</a>
       </li>
-      <li class="links__information">
-        <ul class="information">
+      <li className="links__information">
+        <ul className="information">
           <li>
-            <a class="information__about" href="https://www.isdicoders.com">
+            <a className="information__about" href="https://www.isdicoders.com">
               About
             </a>
           </li>
           <li>
-            <a class="information__service" href="https://www.isdicoders.com">
+            <a
+              className="information__service"
+              href="https://www.isdicoders.com"
+            >
               Service
             </a>
           </li>
           <li>
-            <a class="information__news" href="https://www.isdicoders.com">
+            <a className="information__news" href="https://www.isdicoders.com">
               Company News
             </a>
           </li>
         </ul>
       </li>
-      <li class="links__home">
+      <li className="links__home">
         <a href="home">Cocktails</a>
       </li>
     </ul>

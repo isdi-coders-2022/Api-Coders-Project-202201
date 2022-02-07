72e41b54876bb1c2dc67c1d3c5d5fbc64be733f9
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-02-07 22:02:05 +0100

    fixed type errors in className

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,20 +1,20 @@
 const Navigation = () => {
   return (
-    <nav class="navigation">
-      <ul class="navigation__list">
-        <li class="navigation__item home">
-          <i class="fas fa-home navigation__icon"></i>
-          <span class="navigation__text">Home</span>
+    <nav className="navigation">
+      <ul className="navigation__list">
+        <li className="navigation__item home">
+          <i className="fas fa-home navigation__icon"></i>
+          <span className="navigation__text">Home</span>
         </li>
 
-        <li class="navigation__item glass">
-          <i class="fas fa-glass-martini-alt navigation__icon"></i>
-          <span class="navigation__text">My Bar</span>
+        <li className="navigation__item glass">
+          <i className="fas fa-glass-martini-alt navigation__icon"></i>
+          <span className="navigation__text">My Bar</span>
         </li>
 
-        <li class="navigation__item add">
-          <i class="fas fa-plus navigation__icon"></i>
-          <span class="navigation__text">Create My Cocktail</span>
+        <li className="navigation__item add">
+          <i className="fas fa-plus navigation__icon"></i>
+          <span className="navigation__text">Create My Cocktail</span>
         </li>
       </ul>
     </nav>

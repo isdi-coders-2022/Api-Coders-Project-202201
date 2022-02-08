9f215621449788c8a43a5731afe51405d9f65fa0
Inna Iazovkaia <inna.0961@mail.ru>
2022-02-08 15:29:14 +0100

    added CocktailDetails at App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Header from "./components/Header/Header";
 import Navigation from "./components/Navigation/Navigation";
 import Footer from "./components/Footer/Footer";
+import CocktailDetails from "./components/CocktailDetails/CocktailDetails";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <main>
+        <CocktailDetails />
         <Navigation />
       </main>
       <Footer />

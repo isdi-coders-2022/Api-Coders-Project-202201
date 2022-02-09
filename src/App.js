06d8e2955f47a83fe4833f7cdf98dd3e35b36359
Emiliano Polanco <emilianopolanco5@gmail.com>
2022-02-09 18:28:23 +0100

    removing unused imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import Header from "./components/Header/Header";
 import Navigation from "./components/Navigation/Navigation";
 import Footer from "./components/Footer/Footer";
-import CocktailDetails from "./components/CocktailDetails/CocktailDetails";
 import Button from "./components/Button/Button";
 import { useEffect } from "react";
 import useAPI from "./hooks/useApi";

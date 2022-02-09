bbbf88cbe1938196f6afb1efe279ad6616cb07fd
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-02-09 11:59:09 +0100

    Added more expected outputs in the already present test

--- src/components/Navigation/Navigation.test.js
+++ src/components/Navigation/Navigation.test.js
@@ -3,12 +3,18 @@ import Navigation from "./Navigation";
 
 describe("Given a Navigation component", () => {
   describe("When it's rendered", () => {
-    test("Then it should display a link with 'Home' as text", () => {
+    test("Then it should display links with 'Home' 'My Bar' and 'Create My Cocktail' as text", () => {
       render(<Navigation />);
 
-      const navigation = screen.queryByRole("link", { name: "Home" });
+      const navigationHome = screen.queryByRole("link", { name: "Home" });
+      const navigationMyBar = screen.queryByRole("link", { name: "My Bar" });
+      const navigationCreateMyCocktail = screen.queryByRole("link", {
+        name: "Create My Cocktail",
+      });
 
-      expect(navigation).toBeInTheDocument();
+      expect(navigationHome).toBeInTheDocument();
+      expect(navigationMyBar).toBeInTheDocument();
+      expect(navigationCreateMyCocktail).toBeInTheDocument();
     });
   });
 });

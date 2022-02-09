d89fecde63f4c15ac3522dcf283463c24e023b91
Inna Iazovkaia <inna.0961@mail.ru>
2022-02-09 13:15:47 +0100

    added tests in CocktailDetails.test.js

--- src/components/CocktailDetails/CocktailDetails.test.js
+++ src/components/CocktailDetails/CocktailDetails.test.js
@@ -2,14 +2,58 @@ import CocktailDetails from "./CocktailDetails";
 import { render, screen } from "@testing-library/react";
 
 describe("Given a CocktailDetails component", () => {
-  describe("When it receives name 'Margarita'", () => {
-    test("Then it should display 'Margarita'", () => {
-      render(<CocktailDetails />);
-      const expectedText = "Margarita";
+  describe("When it receives cocktail 'Margarita'", () => {
+    test("Then it should display h1 with text 'Margarita'", () => {
+      const cocktail = {
+        name: "Margarita",
+        imgURL: "",
+        category: "",
+        ingredients: "",
+        measure: "",
+        instructions: "",
+      };
 
-      const p = screen.queryByText(expectedText);
+      render(<CocktailDetails cocktail={cocktail} />);
 
-      expect(p).toBeInTheDocument();
+      const h1 = screen.queryByRole("heading", { name: "Margarita" });
+
+      expect(h1).toBeInTheDocument();
+    });
+  });
+
+  describe("When it receives cocktail with category 'Alcoholic'", () => {
+    test("Then it should display p with text 'Category: Alcoholic'", () => {
+      const cocktail = {
+        name: "",
+        imgURL: "",
+        category: "Alcoholic",
+        ingredients: "",
+        measure: "",
+        instructions: "",
+      };
+
+      render(<CocktailDetails cocktail={cocktail} />);
+      const paragraph = screen.getByText("Category: Alcoholic");
+
+      expect(paragraph).toBeInTheDocument();
+    });
+  });
+
+  describe("When it receives cocktail 'Mary'", () => {
+    test("Then it should display img with alt 'Mary'", () => {
+      const cocktail = {
+        name: "Mary",
+        imgURL: "",
+        category: "",
+        ingredients: "",
+        measure: "",
+        instructions: "",
+      };
+
+      render(<CocktailDetails cocktail={cocktail} />);
+      const alt = screen.queryByAltText("Mary");
+
+      expect(alt).toBeInTheDocument();
     });
   });
 });

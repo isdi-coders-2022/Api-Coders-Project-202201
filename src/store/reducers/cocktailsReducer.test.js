0c77f140a1a585f20fcb71654ff801f9a7dbb0fb
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-02-09 13:04:00 +0100

    Added test to cocktailsReducer to test the default case

--- src/store/reducers/cocktailsReducer.test.js
+++ src/store/reducers/cocktailsReducer.test.js
@@ -2,7 +2,7 @@ import actionsTypes from "../actions/cocktails/actionsTypes";
 import cocktailsReducer from "./cocktailsReducer";
 
 describe("Given a cocktailsReducer function", () => {
-  describe("When it receives 'Absinthe #2' and 'Amaretto Liqueur' objects", () => {
+  describe("When it receives 'cocktails' with 'Absinthe #2' and 'Amaretto Liqueur' objects and 'loadCocktailsAction' action", () => {
     test("Then it should return 'Absinthe #2' and 'Amaretto Liqueur' objects", () => {
       const cocktails = [
         {
@@ -34,6 +34,41 @@ describe("Given a cocktailsReducer function", () => {
 
       const cocktailsList = cocktailsReducer(cocktails, action);
 
+      expect(cocktailsList).toEqual(expectedCocktails);
+    });
+  });
+
+  describe("When it receives 'cocktails' with 'Absinthe #2' and 'Amaretto Liqueur' objects and 'doSomethingAction' action", () => {
+    test("Then it should return an empty array", () => {
+      const currentCocktails = [
+        {
+          strDrink: "Absinthe #2",
+          strDrinkThumb: "",
+          idDrink: "12790",
+        },
+        {
+          strDrink: "Amaretto Liqueur",
+          strDrinkThumb: "",
+          idDrink: "12792",
+        },
+      ];
+      const action = {
+        type: actionsTypes.doSomethingAction,
+      };
+      const expectedCocktails = [
+        {
+          strDrink: "Absinthe #2",
+          strDrinkThumb: "",
+          idDrink: "12790",
+        },
+        {
+          strDrink: "Amaretto Liqueur",
+          strDrinkThumb: "",
+          idDrink: "12792",
+        },
+      ];
+
+      const cocktailsList = cocktailsReducer(currentCocktails, action);
       expect(cocktailsList).toEqual(expectedCocktails);
     });
   });

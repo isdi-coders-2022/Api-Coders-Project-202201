0da026f6f308fb7a98aad3571e2f0863b23b8518
Inna Iazovkaia <inna.0961@mail.ru>
2022-02-09 10:48:17 +0100

    created styled-components in CocktailDetails.js

--- src/components/CocktailDetails/CocktailDetails.js
+++ src/components/CocktailDetails/CocktailDetails.js
@@ -1,4 +1,33 @@
+import styled from "styled-components";
 import "./CocktailDetails.css";
+
+const CoctailDescription = styled.section`
+  font-family: Recursive;
+  margin-bottom: 100px;
+`;
+
+const CocktailName = styled.h1`
+  font-size: 24px;
+  font-family: Piazzolla;
+  font-weight: normal;
+  padding-left: 20px;
+`;
+
+const ImageContainer = styled.div`
+  width: 100vw;
+  height: 200px;
+  overflow: hidden;
+`;
+
+const CocktailImage = styled.img`
+  width: 100vw;
+`;
+
+const DescriptionContainer = styled.div`
+  padding-left: 10px;
+  padding-right: 20px;
+  font-size: 15px;
+`;
 
 const CocktailDetails = () => {
   const name = "Margarita";
@@ -11,12 +40,16 @@ const CocktailDetails = () => {
     "Rub the rim of the glass with the lime slice to make the salt stick to it. Take care to moisten only the outer rim and sprinkle the salt on it. The salt should present to the lips of the imbiber and never mix into the cocktail. Shake the other ingredients with ice, then carefully pour into the glass.";
 
   return (
-    <section className="cocktail-description">
-      <p className="cocktail-description__name">{name}</p>
-      <div className="image-container">
-        <img className="cocktail-description__image" src={imgURL} alt={name} />
-      </div>
-      <div className="description-container">
+    <CoctailDescription className="cocktail-description">
+      <CocktailName className="cocktail-description__name">{name}</CocktailName>
+      <ImageContainer className="image-container">
+        <CocktailImage
+          className="cocktail-description__image"
+          src={imgURL}
+          alt={name}
+        />
+      </ImageContainer>
+      <DescriptionContainer className="description-container">
         <p className="cocktail-description__category">Category: {category}</p>
         <p className="cocktail-description__ingredients">
           Ingredient: {ingredients} {measure}
@@ -24,8 +57,8 @@ const CocktailDetails = () => {
         <p className="cocktail-description__instructions">
           Instractions: {instructions}
         </p>
-      </div>
-    </section>
+      </DescriptionContainer>
+    </CoctailDescription>
   );
 };
 

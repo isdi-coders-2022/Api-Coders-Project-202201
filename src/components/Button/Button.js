3f582740de57f7537bba5df4ed1d844eae5722dc
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-02-10 22:42:44 +0100

    Refactorised Button component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,36 +1,16 @@
 import styled from "styled-components";
 
-const SimpleButton = styled.button`
-  background-color: #7d4646;
+const StarButton = styled.button`
+  background-color: transparent;
   color: white;
   height: 50px;
   width: 100px;
   border: none;
+  font-size: 30px;
 `;
 
-const ButtonStar = styled.button`
-  border: none;
-  width: 33.33px;
-  height: 31.67px;
-  font-size: 31px;
-  text-align: center;
-  padding: 0;
-  color: #fff;
-  background-color: transparent;
-`;
-
-const Button = ({ text, action, type }) => {
-  return (
-    <>
-      {type === "star" && <ButtonStar onClick={action}>{text}</ButtonStar>}
-
-      {type === "simple" && (
-        <SimpleButton className="simple-button" onClick={action}>
-          {text}
-        </SimpleButton>
-      )}
-    </>
-  );
+const Button = ({ text, actionOnClick }) => {
+  return <StarButton onClick={actionOnClick}>{text}</StarButton>;
 };
 
 export default Button;

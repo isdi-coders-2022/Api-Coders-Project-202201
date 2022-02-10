d1badf99becf96acc99588de6cf408838e47762c
Emiliano Polanco <emilianopolanco5@gmail.com>
2022-02-10 11:15:30 +0100

    placing text a background img and a button to the homepage

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,9 +1,54 @@
+import styled from "styled-components";
+const ExploreButton = styled.button`
+  background-color: #7d4646;
+  border: none;
+  color: white;
+  width: 100px;
+  height: 50px;
+  margin-top: 100px;
+`;
+const BackgroundPage = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100vw;
+  height: 90vh;
+  overflow: hidden;
+`;
+const Image = styled.img`
+  width: auto;
+  height: 100%;
+  position: absolute;
+  z-index: -1;
+  @media (min-width: 600px) {
+    width: 100vw;
+    height: auto;
+    transform: translate(0, -300px);
+  }
+`;
+
+const SimpleText = styled.p`
+  margin-top: 100px;
+  color: white;
+  width: 80%;
+`;
+
 const HomePage = () => {
   return (
     <>
-      <div>
-        <p>A</p>
-      </div>
+      <BackgroundPage>
+        <SimpleText>
+          Because everything starts with a drink , explore ours
+        </SimpleText>
+        <ExploreButton>Let's see</ExploreButton>
+        <Image
+          className="HomePage__background"
+          src={require("../../images/background-home.jpg")}
+          alt="background-cocktails"
+        />
+      </BackgroundPage>
+      <h2>Categories</h2>
     </>
   );
 };

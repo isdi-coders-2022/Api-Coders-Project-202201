6cfebba160c5060ab51085851498db11a7c20c16
Inna Iazovkaia <inna.0961@mail.ru>
2022-02-10 23:44:48 +0100

    uppdated MyBarPage.test.js

--- src/pages/MyBarPage/MyBarPage.test.js
+++ src/pages/MyBarPage/MyBarPage.test.js
@@ -1,10 +1,28 @@
 import MyBarPage from "./MyBarPage";
 import { render, screen } from "@testing-library/react";
+import CocktailDataContextProvider from "../../store/contexts/CocktailDataContextProvider";
 
 describe("Given a MyBarPage component", () => {
-  describe("When it's rendered'", () => {
+  describe("When it receives 'cocktails' and it's rendered'", () => {
     test("Then it should display a title 'My Bar'", () => {
-      render(<MyBarPage />);
+      const cocktails = [
+        {
+          strDrink: "Monica",
+          strDrinkThumb: "",
+          idDrink: "1",
+        },
+        {
+          strDrink: "Menchu",
+          strDrinkThumb: "",
+          idDrink: "2",
+        },
+      ];
+
+      render(
+        <CocktailDataContextProvider value={cocktails}>
+          <MyBarPage />
+        </CocktailDataContextProvider>
+      );
       const expectedText = "My Bar";
 
       const text = screen.queryByRole("heading", expectedText);
